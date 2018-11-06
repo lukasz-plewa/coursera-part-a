e8ac03ae8dc887eecc0a289dcc89bae6f647e7b6
Lukasz Plewa <L.Plewa@adbglobal.com>
2018-11-06 11:15:48 +0100

    Final homework A1

--- coursera-A1.0/src/coursera-A1.0.cpp
+++ coursera-A1.0/src/coursera-A1.0.cpp
@@ -1,2 +1,25 @@
+
 #include <iostream>
 #include <vector>
+using namespace std;
+
+const unsigned int elements_size   = 40;
+
+template <class elem_type>
+elem_type sum(vector<elem_type> &table){
+    elem_type accumulate = 0;
+    for (auto el : table)
+        accumulate += el;
+    return accumulate;
+}
+
+int main()
+{
+    vector<int> elements;
+
+    for (unsigned int i=0; i < elements_size; i++)
+        elements.push_back(i);
+
+    cout << "Sum of " << elements_size << " elements is " << sum(elements) << endl;
+    return 0;
+}
